ddbf99e23563f0f61b63d27ce8f177b6cc7e57c8
helloangusmok <34521800+helloangusmok@users.noreply.github.com>
2018-05-09 21:55:54 +1000

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var menuIcon = document.getElementById('menu-icon');
+
+function transitionIcon(){
+	if(menuIcon.classList.contains('fa-bars')){
+		menuIcon.classList.remove('fa-bars');
+		menuIcon.classList.add('fa-times');
+		document.getElementById('mobile-menu').style.width = '100%';
+		document.querySelector('body').style.overflow = 'hidden';
+	} else if(menuIcon.classList.contains('fa-times')){
+		menuIcon.classList.remove('fa-times');
+		menuIcon.classList.add('fa-bars');
+		document.getElementById('mobile-menu').style.width = '0';
+		document.querySelector('body').style.overflow = 'scroll';
+	}
+}
+
+function getTestimonial() {
+	
+}
+
+document.getElementsByClassName('small-menu')[0].addEventListener('click', function(){
+	transitionIcon();
+});
